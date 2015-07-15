0e62c80ee4554eb482baa301531eff2d67f22afd
Tom Fewster <tom@wannabegeek.com>
2015-07-15 06:56:07 +0100

    more

--- utils/histdata_fx_download.py
+++ utils/histdata_fx_download.py
@@ -10,9 +10,12 @@ values = {'tk' : 'c92691404446db5f82102a9cab029052',
         'fxpair' : 'EURUSD'
         }
 
+headers = { 'User-Agent' : 'Mozilla/5.0' }
+
 data = urllib.parse.urlencode(values)
 data = data.encode('utf-8') # data should be bytes
-req = urllib.request.Request(url, data)
+req = urllib.request.Request(url, data = data, headers = headers)
 with urllib.request.urlopen(req) as response:
+    print("Response Code: %s\nInfo: %s\n" % (response.getcode(), response.info()))
     the_page = response.read()
     print(the_page)
